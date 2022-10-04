26d2b9102a47827d1d87756a4ecbb119a9006fd2
kasunmadhumal <skasunmk982@gmail.com>
2022-10-04 13:32:43 +0530

    change the configuration files in git

--- backend/src/main/java/com/kelaniya/backend/configuration/auth/CorsConfig.java
+++ backend/src/main/java/com/kelaniya/backend/configuration/auth/CorsConfig.java
@@ -6,10 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
-  private static final String GET = "GET";
-  private static final String POST = "POST";
-  private static final String PUT = "PUT";
-  private static final String DELETE = "DELETE";
+ 
 
 
     @Override
